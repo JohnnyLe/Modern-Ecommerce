1f9135acda69acf879f0d2afcbc4213343342da6
hung.duong@nit-software.com <hung.duong@nit-software.com>
2016-09-13 15:40:22 +0700

    update

--- backend-rest-api/src/main/java/com/nitsoft/ecommerce/api/orders/OrdersAPI.java
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/api/orders/OrdersAPI.java
@@ -19,9 +19,8 @@ import com.nitsoft.util.UniqueID;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.hibernate.validator.constraints.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -49,11 +48,8 @@ public class OrdersAPI extends APIUtil {
     @RequestMapping(method = RequestMethod.POST, produces = APIName.CHARSET)
     @ResponseBody
     public String addOrders(@RequestParam(name = "user_id", required = false) String userId,
-            @RequestParam(name = "email", required = false) String email,
-            @RequestParam(name = "create_date", required = true) String createDate,
+            @RequestParam(name = "email", required = false) @Email String email,
             @RequestParam(name = "company_id", required = true) Long companyId,
-            @RequestParam(name = "created_at", required = true) String createdAt,
-            @RequestParam(name = "updated_at", required = false) String updatedAt,
             @RequestParam(name = "is_active", required = false) Short isActive,
             @RequestParam(name = "is_virtual", required = false) Short isVirtual,
             @RequestParam(name = "is_multi_shipping", required = false) Short isMultiShipping,
@@ -63,53 +59,67 @@ public class OrdersAPI extends APIUtil {
             @RequestParam(name = "grand_total", required = false) BigDecimal grandTotal,
             @RequestParam(name = "base_grand_total", required = false) BigDecimal baseGrandTotal,
             @RequestParam(name = "checkout_comment", required = false) String checkoutComment,
-            @RequestParam(name = "customer_email", required = false) String customerEmail,
-            @RequestParam(name = "customer_prefix", required = false) String customerPrefix,
-            @RequestParam(name = "customer_firstname", required = false) String customerFirstname,
-            @RequestParam(name = "customer_middlename", required = false) String customerMiddlename,
-            @RequestParam(name = "customer_lastname", required = false) String customerLastname,
-            @RequestParam(name = "customer_suffix", required = false) String customerSuffix,
-            @RequestParam(name = "customer_dob", required = false) String customerDob,
+            @RequestParam(name = "customer_email", required = true) String customerEmail,
+            @RequestParam(name = "customer_prefix", required = true) String customerPrefix,
+            @RequestParam(name = "customer_firstname", required = true) String customerFirstname,
+            @RequestParam(name = "customer_middlename", required = true) String customerMiddlename,
+            @RequestParam(name = "customer_lastname", required = true) String customerLastname,
+            @RequestParam(name = "customer_suffix", required = true) String customerSuffix,
+            @RequestParam(name = "customer_dob", required = true) String customerDob,
             @RequestParam(name = "customer_is_guest", required = false) Short customerIsGuest,
             @RequestParam(name = "remote_ip", required = false) String remoteIp,
             @RequestParam(name = "customer_gender", required = false) String customerGender,
             @RequestParam(name = "subtotal", required = false) BigDecimal subtotal,
             @RequestParam(name = "base_subtotal", required = false) BigDecimal baseSubtotal,
             @RequestParam(name = "is_changed", required = false) Integer isChanged) {
-        Date createday;
-        Date updateday;
-        Date customerbirthday;
-        Date create_date;
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
-        try {
-            createday = dateFormat.parse(createdAt);
-            updateday = dateFormat.parse(updatedAt);
-            customerbirthday = dateFormat.parse(customerDob);
-            create_date=dateFormat.parse(createDate);
-        } catch (ParseException e) {
-            throw new ApplicationException(APIStatus.INVALID_PARAMETER);
-        }
+
+        Date createDate = new Date();
+//        Date customerbirthday;
+//        Date create_date;
+        
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+//        try {
+//            createday = dateFormat.parse(new String createdAt);
+//            updateday = dateFormat.parse(updatedAt);
+//            customerbirthday = dateFormat.parse(customerDob);
+//            create_date = dateFormat.parse(createDate);
+//        } catch (ParseException e) {
+//            throw new ApplicationException(APIStatus.INVALID_PARAMETER);
+//        }
 
         Orders orders = new Orders();
         User users = new User();
-//        orders.setId(id);
+
+        // validate user id
         if (userId == null || userId.equals("")) {
+            // validate email
+            if (email != null) {
+                // create anonymous user
+                users.setUserId(UniqueID.getUUID());
+                users.setCompanyId(companyId);
+                users.setCreateDate(createDate);
+                users.setRoleId(Constant.USER_ROLE.ANONYMOUS_USER.getRoleId());
+                users.setPasswordHash(UniqueID.getUUID());
+                users.setFirstName("");
+                users.setLastName("");
+                users.setStatus(Constant.USER_STATUS.PENDING.getStatus());
+                users.setSalt(UniqueID.getUUID());
+                customerService.save(users);
+            } else {
+                throw new ApplicationException(APIStatus.INVALID_PARAMETER, new IllegalArgumentException("Email address is null"));
+            }
+        } else {
             
-            users.setUserId(UniqueID.getUUID());
-            users.setCompanyId(companyId);
-            users.setCreateDate(createday);
-            users.setEmail(email);
-            customerService.save(users);
         }
-        
+
         orders.setUserId(users.getUserId());
         orders.setCompanyId(companyId);
-        orders.setCreatedAt(createday);
-        orders.setUpdatedAt(updateday);
+//        orders.setCreatedAt(createday);
+//        orders.setUpdatedAt(updateday);
         orders.setIsActive(isActive);
         orders.setIsVirtual(isVirtual);
         orders.setIsMultiShipping(isMultiShipping);
-        orders.setStatus(status);
+        orders.setStatus(Constant.ORDER_STATUS.PENDING.getStatus());
         orders.setItemsCount(itemsCount);
         orders.setItemsQuantity(itemsQuantity);
         orders.setGrandTotal(grandTotal);
@@ -121,7 +131,7 @@ public class OrdersAPI extends APIUtil {
         orders.setCustomerMiddlename(customerMiddlename);
         orders.setCustomerLastname(customerLastname);
         orders.setCustomerSuffix(customerSuffix);
-        orders.setCustomerDob(customerbirthday);
+//        orders.setCustomerDob(customerbirthday);
         orders.setCustomerIsGuest(customerIsGuest);
         orders.setRemoteIp(remoteIp);
         orders.setCustomerGender(customerGender);
@@ -131,7 +141,6 @@ public class OrdersAPI extends APIUtil {
 
         ordersService.save(orders);
         return writeObjectToJson(new StatusResponse<>(HttpStatus.OK.value(), orders));
-
     }
 
     @ApiOperation(value = "get orders by company id", notes = "")
--- backend-rest-api/src/main/java/com/nitsoft/util/Constant.java
+++ backend-rest-api/src/main/java/com/nitsoft/util/Constant.java
@@ -99,6 +99,38 @@ public class Constant {
             return roleName;
         }
     }
+    
+    public static enum USER_STATUS {
+        INACTIVE(-1),
+        PENDING(0),
+        ACTIVE(1);
+        
+        private final int status;
+        
+        private USER_STATUS(int status) {
+            this.status = status;
+        }
+        
+        public int getStatus() {
+            return status;
+        }
+    }
+    
+    public static enum ORDER_STATUS {
+        PENDING(0),
+        SHIPPING(1),
+        COMPLETED(2);
+        
+        private final int status;
+        
+        private ORDER_STATUS(int status) {
+            this.status = status;
+        }
+        
+        public int getStatus() {
+            return status;
+        }
+    }
 
     public static final long ONE_MINUTE_IN_MILLIS = 60000;
     public static final long ONE_SECOND_IN_MILLIS = 1000;
