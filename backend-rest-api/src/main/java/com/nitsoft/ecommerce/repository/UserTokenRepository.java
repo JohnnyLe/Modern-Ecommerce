0471ba142f6c33259e2a169ac821406d90ec42ce
hung.duong@nit-software.com <hung.duong@nit-software.com>
2016-09-14 14:02:52 +0700

    update code

--- backend-rest-api/src/main/java/com/nitsoft/ecommerce/repository/UserTokenRepository.java
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/repository/UserTokenRepository.java
@@ -6,7 +6,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserTokenRepository extends CrudRepository<UserToken, String> {
-    @Query("SELECT u FROM UserToken u WHERE u.userId = :userId AND u.companyId = :companyId")
-    UserToken findExpirationDate(@Param("userId") String userId, @Param("companyId") long companyId);
 
 }
