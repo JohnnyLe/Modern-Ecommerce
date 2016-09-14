fafaa4a8ff48632984a9a7eb9bf0d959367f5f2b
linh.do@nit-software.com <linh.do@nit-software.com>
2016-09-14 11:41:30 +0700

    fix user login API

--- backend-rest-api/src/main/java/com/nitsoft/ecommerce/repository/UserRepository.java
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.nitsoft.ecommerce.repository;
 
 import com.nitsoft.ecommerce.database.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends PagingAndSortingRepository<User, String> {
 
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.companyId = :companyId")
     User findByEmail(@Param("email") String email, @Param("companyId") long companyId);
 
 }
