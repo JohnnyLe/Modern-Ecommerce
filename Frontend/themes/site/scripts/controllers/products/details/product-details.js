3669161ae8d6de383782b9b9b5eb866402fc0ffe
hung.duong@nit-software.com <hung.duong@nit-software.com>
2016-09-15 18:20:49 +0700

    review submit

--- Frontend/themes/site/scripts/controllers/products/details/product-details.js
+++ Frontend/themes/site/scripts/controllers/products/details/product-details.js
@@ -4,7 +4,8 @@ angular.module('marketplace.products.details', [])
 
 
 .controller('ProductDetailsCtrl', [ '$scope', 'util', '$', '$timeout', '$stateParams', function( $scope, util, $, $timeout, $stateParams) {
-        
+    $scope.proComment = {};
+         
     // Data model biding
     $scope.loadData = function () {
         util.callRequest('products/' + $stateParams.productId, "GET").then(function (data) {
@@ -13,6 +14,14 @@ angular.module('marketplace.products.details', [])
         
         util.callRequest('reviews/' + $stateParams.productId, "GET").then(function (data) {
             $scope.reviews = data.result;
+        });
+    };
+    
+    $scope.insertComment = function () {
+        $scope.proComment.productId = $stateParams.productId;
+        $scope.proComment.rank = 1;
+        
+        util.callRequest('reviews/add?token=' + "dfasdfasdf", "POST", $scope.proComment).then(function (data) {
         });
     };
 
@@ -25,29 +34,6 @@ angular.module('marketplace.products.details', [])
         });
     });
 }]);
-=======
-        .controller('ProductDetailsCtrl', ['$scope', 'util', '$', '$timeout', '$stateParams', function ($scope, util, $, $timeout, $stateParams) {
-
-                // Data model biding
-                $scope.loadData = function () {
-                    util.callRequest('products/' + $stateParams.productId, "GET").then(function (data) {
-                        $scope.product = data.result.product;
-                        if (data.result.attributes) {
-                            $scope.productImgs = JSON.parse(data.result.attributes.value_string);
-                        }
-                    });
-                };
-
-                $scope.loadData();
-
-                $timeout(function () {
-                    // init slider
-                    $('#similar-product').carousel({
-                        interval: 5000
-                    });
-                });
-            }]);
->>>>>>> ec975ab532b346b1f7cf9e550040a08fb03ac090
 
 
 
