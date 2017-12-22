f678f1116fad02013c32613cfc9d90d4ea81f455
tung nguyen <tung.nguyen@nit-software.com>
2017-12-22 19:22:54 +0700

    update

---
+++ backend-rest-api/src/main/java/com/nitsoft/ecommerce/api/request/model/CreateCategoryRequestModel.java
@@ -0,0 +1,28 @@
+package com.nitsoft.ecommerce.api.request.model;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class CreateCategoryRequestModel {
+
+    private long companyId;
+
+    private long parentId;
+
+    @NotNull
+    @NotBlank
+    private String name;
+
+    @Min(0)
+    @Max(1)
+    private int status;
+
+    private int position;
+
+    private String description;
+}
