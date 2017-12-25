1ba0b5f05a2f8857ffb8c4ba697417ae520fb695
trinhlbk <trinh@nit-software.com>
2017-12-25 20:58:49 +0700

    update Users fronend

---
+++ frontend-admin-site/public_html/js/components/users/edit/users_edit_ctrl.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('ec-admin.app', ['ec-admin'])
+        .controller('UserEditCtrl', [
+
+            '$scope',
+            'Util',
+            'API',
+            '$state',
+            'Patterns',
+            '$stateParams',
+            'AppConfig',
+
+            function ($scope, Util, API, $state, Patterns, $stateParams, AppConfig) {
+                
+            }
+        ]);
