668fb46bfec23e8798443d3b90cfe6e004462b0e
trinhlbk <trinh@nit-software.com>
2018-01-16 21:43:18 +0700

    update

--- frontend-admin-site/public_html/js/components/users/edit/users_edit_ctrl.js
+++ frontend-admin-site/public_html/js/components/users/edit/users_edit_ctrl.js
@@ -13,12 +13,19 @@ angular.module('ec-admin.app', ['ec-admin'])
             'Patterns',
             function ($scope, Util, API, $state, Patterns, $stateParams, AppConfig) {
                 $scope.phonePattern = Patterns.PHONE_PATTERN;
+                $scope.showBntSave = true;
                 $scope.submitting = false;
+
+                var originUser;
+
                 $scope.getUserInfo = function (cb) {
                     Util.createRequest(API.GET_USER_INFO.path + $stateParams.id, function (response) {
                         var status = response.status;
                         if (status === 200) {
                             $scope.userInfo = response.data;
+                            originUser = angular.copy($scope.userInfo);
+
+                            setWatch();
                         } else {
                             Util.showErrorAPI(status);
                         }
@@ -28,19 +35,21 @@ angular.module('ec-admin.app', ['ec-admin'])
                 $scope.getUserInfo();
 
                 $scope.updateUser = function () {
+                    $scope.showBntSave = false;
+                    $scope.submitting = false;
                     var paramUser = {};
                     paramUser.userId = $stateParams.id;
                     paramUser.firstName = $scope.userInfo.firstName;
                     paramUser.lastName = $scope.userInfo.lastName;
-                    paramUser.middleName = $scope.userInfo.middleName,
+                    paramUser.middleName = $scope.userInfo.middleName;
                     paramUser.email = $scope.userInfo.email;
                     paramUser.phone = $scope.userInfo.phone;
                     paramUser.fax = $scope.userInfo.fax;
                     paramUser.address = $scope.userInfo.address;
                     paramUser.city = $scope.userInfo.city;
                     paramUser.country = $scope.userInfo.country;
-                    $scope.submitting = true;
-                    
+
+
                     Util.createRequest(API.EDIT_USER, paramUser, function (response) {
                         var status = response.status;
                         if (status === 200) {
@@ -53,8 +62,14 @@ angular.module('ec-admin.app', ['ec-admin'])
                         }
                     });
                 };
-                
-                $scope.goChangePassword = function(){
+
+                function setWatch() {
+                    $scope.$watch('userInfo', function (newValue) {
+                        $scope.origin = angular.equals(newValue, originUser);
+                    }, true);
+                }
+
+                $scope.goChangePassword = function () {
                     $state.go('users.changePassword', {id: $stateParams.id});
                 };
             }
